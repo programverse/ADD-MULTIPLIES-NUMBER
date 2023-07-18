6f795a5c2901e18ab3418b965c8096ffe3ccfdd8
Biswajeet Nayak <139606176+programverse@users.noreply.github.com>
2023-07-18 10:03:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# print("hi")
-# print("i am biswa \n 'he is a good' boy")
-# print("Marky")
-# print("September 20, 2029")
-#
-# print("""I am signing up for Replit's 100 days of Python challenge!
-# I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
-# I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!""")
-#
-# print("I am feeling 🥳")
-#
-# print("You can follow my progress at replit.com/IamMarkProbably")
-# print("i wan to write that \n you are a good script witer also a good programer \n but that is not be a good enterprenoir but \n it is working or not let's check")
 # Here is how to add 2 number with multiply the numbers .
 num1 =int(input("Enter Number 1 : "))
 num2 =int(input("Enter Number 2 : "))
